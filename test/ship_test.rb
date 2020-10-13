f6c94b130137936de11d69bdb4e99da83744155f
Aiden Mendez <aidenmendez@gmail.com>
2020-10-12 20:04:08 -0600

    Add sunk method Test ship not sunk by default

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -3,8 +3,22 @@ require "Minitest/autorun"
 require "Minitest/pride"
 
 class ShipTest < Minitest::Test
+  def setup
+    @cruiser = Ship.new("cruiser", 3)
+
+  end
+  
   def test_it_exists
-    ship = Ship.new("cruiser", 3)
-    assert_instance_of Ship, ship
+    assert_instance_of Ship, @cruiser
+  end
+
+  def test_ship_attributes
+    assert_equal "cruiser", @cruiser.name
+    assert_equal 3, @cruiser.length
+    assert_equal 3, @cruiser.health
+  end
+
+  def test_ship_not_sunk_by_default
+    assert_equal false, @cruiser.sunk?
   end
 end
