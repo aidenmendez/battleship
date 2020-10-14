bc839a78cb6df899a1064f2cc86b841513efe62d
Max Ribbans <ribbans.max@gmail.com>
2020-10-13 22:04:26 -0400

    Update setup for test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -6,27 +6,30 @@ require "./lib/ship"
 
 class CellTest < Minitest::Test
   def setup
+    @cell.place_ship(@cruiser)
+    @cell_empty = Cell.new("Z100")
     @cell = Cell.new("A1")
     @cruiser = Ship.new("cruiser",3)
   end
 
   def test_it_exists
-    assert_instance_of Cell, @cell
+    assert_instance_of Cell, @cell_empty
   end
 
   def test_attributes_exist
-    assert_equal nil, @cell.ship
-    assert_equal "A1", @cell.coordinate
+    assert_equal nil, @cell_empty.ship
+    assert_equal "A1", @cell_empty.coordinate
   end
 
   def test_cell_empty_by_default
-    assert_equal true, @cell.empty?
+    assert_equal true, @cell_empty.empty?
   end
 
   def test_can_place_ship_in_cell
     assert_equal true, @cell.empty?
-    @cell.place_ship(@cruiser)
     assert_equal "cruiser", @cell.ship.name
     assert_equal false, @cell.empty?
   end
+
+
 end
