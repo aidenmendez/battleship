e9becec5b5fa96d746d67185a68eb5c07adbf36e
Max Ribbans <ribbans.max@gmail.com>
2020-10-13 21:35:30 -0400

    Test cell attributes exist successfully

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -1,14 +1,21 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/cell"
+require "./lib/ship"
 
 
 class CellTest < Minitest::Test
   def setup
     @cell = Cell.new("A1")
+    @cruiser = Ship.new("cruiser",3)
   end
 
   def test_it_exists
     assert_instance_of Cell, @cell
+  end
+
+  def test_attributes_exist
+    assert_equal nil, @cell.ship
+    assert_equal "A1", @cell.coordinate
   end
 end
