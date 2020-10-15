04656652df6364e877bb4063882b0e012ef7c717
Aiden Mendez <aidenmendez@gmail.com>
2020-10-14 18:15:24 -0600

    Add setup method to test file and test board class exists

---
+++ test/board_test.rb
@@ -0,0 +1,13 @@
+require "Minitest/autorun"
+require "Minitest/pride"
+require "./lib/board"
+require "./lib/cell"
+
+class BoardTest < Minitest::Test
+  def setup
+    @board = Board.new
+  end
+  def test_it_exists
+    assert_instance_of Board, @board
+  end
+end
