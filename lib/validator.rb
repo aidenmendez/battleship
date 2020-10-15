2d433ee2f3079670edd19de5a069d3fb4196e708
Max Ribbans <ribbans.max@gmail.com>
2020-10-15 17:08:19 -0400

    Update pseudocode

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,3 +1,5 @@
+
+
 #### 
 class Validator
     #Psuedo code:
@@ -12,7 +14,38 @@ class Validator
     #                     return :horizontal = true
     #       return a symbol, either :horizontal (incremental) or :vertical (same)
     #           if same, :vertical
-    #           if not same, :horizontal
+    #           if not same, :horizontal 
+    # 
+    # 
+    # number_validator that takes a coord []
+    #   number_validator extracts numbers into []
+    #       check range of number []
+    #       check numbers are all same or all incremental
+    #           if !same, check incremental by 1 only
+    #                   use each_cons()
+    #                   if arr[0].each_cons(arr.length) == arr
+    #                     return :horizontal = true
+    #       return a symbol, either :horizontal (same) or :vertical (incremental)
+    #           if same, :horizontal
+    #           if not same, :vertical
+    # 
+    # 
+    # 
+    # check_coordinates method (user inputs are kosher)
+    #   this method is used for coordinate validation, 
+    #       length? 
+    #       letter_validator
+    #       number_validator
+    #       
+    # 
+    # check_placement method (check ships can be placed)
+    #   this method is used to verify a ship can be placed in the given coords.
+    #       check_coordinates
+    #       check_unoccupied
+    #       return true
+    # 
+    # 
+    
 
 
 
