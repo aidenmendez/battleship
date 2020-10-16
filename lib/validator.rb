9d6e7cf7e705b486d53e4af7bd1eb8314fa895a8
Aiden Mendez <aidenmendez@gmail.com>
2020-10-15 19:00:49 -0600

    Clean up validate numbers method

--- lib/validator.rb
+++ lib/validator.rb
@@ -2,79 +2,92 @@
 
 #### 
 class Validator
-    attr_reader :coordinates, :length 
+  attr_reader :coordinates, :length 
 
-    def initialize(coordinates, length)
-        @coordinates = coordinates
-        @length = length
-        @acceptable_coords = cells = [
-            "A1",
-            "A2",
-            "A3",
-            "A4",
-            "B1",
-            "B2",
-            "B3",
-            "B4",
-            "C1",
-            "C2",
-            "C3",
-            "C4",
-            "D1",
-            "D2",
-            "D3",
-            "D4"
-          ]
-    end 
+  def initialize(coordinates, length)
+    @coordinates = coordinates
+    @length = length
+    @acceptable_coords = cells = [
+      "A1",
+      "A2",
+      "A3",
+      "A4",
+      "B1",
+      "B2",
+      "B3",
+      "B4",
+      "C1",
+      "C2",
+      "C3",
+      "C4",
+      "D1",
+      "D2",
+      "D3",
+      "D4"
+    ]
+  end 
 
-    def length?()
-        @length == @coordinates.length
+  def length?()
+    @length == @coordinates.length
+  end
+
+  def acceptable_coords?
+    thing_to_return = true
+    @coordinates.each do |coordinate|
+      if @acceptable_coords.include?(coordinate)
+      else
+      thing_to_return = false
+      end
     end
+    thing_to_return
+  end
 
-    def acceptable_coords?
-        thing_to_return = true
-        @coordinates.each do |coordinate|
-          if @acceptable_coords.include?(coordinate)
-          else
-            thing_to_return = false
-          end
-        end
-        thing_to_return
-    end
-
-    def validate_letters
-        letters = []
+  def validate_letters
+    letters = []
     
-        @coordinates.each do |coordinate|
+    @coordinates.each do |coordinate|
           # coordinate is "A1"
-          letters << coordinate[0]
-        end
-        if letters.all? {|letter| letter == letters[0]}
-          "vertical"
-        elsif letters.all?((letters[0].ord)..(letters[0].ord + @length - 1))
-          "horizontal"
-        else
-          nil
-        end
-    
+      letters << coordinate[0]
     end
 
-    def validate_numbers
-        numbers = []
-    
-        @coordinates.each do |coordinate|
-          # coordinate is "A1"
-          numbers << coordinate.delete("A-Z")
-        end
-        if numbers.all? {|number| number == numbers[0]}
-          "horizontal"
-        elsif numbers.all?((numbers[0].to_i..(numbers[0].to_i + @length - 1)))
-          "vertical"
-        else
-          nil
-        end
-    
+    letters = letters.map do |let|
+      let.ord
     end
+
+    if letters.all? {|letter| letter == letters[0]}
+      0
+    elsif letters.uniq.length != letters.length
+      2
+    elsif letters.all?((letters[0])..(letters[0] + @length - 1))
+      require 'pry'; binding.pry
+      1
+    else
+      2
+    end
+  end
+
+  def validate_numbers
+    numbers = []
+
+    @coordinates.each do |coordinate|
+      # coordinate is "A1"
+      numbers << coordinate.delete("A-Z")
+    end
+
+    numbers = numbers.map do |num|
+      num.to_i
+    end
+
+    if numbers.all? {|number| number == numbers[0]}
+      0
+    elsif numbers.uniq.length != numbers.length
+      2
+    elsif numbers.all?((numbers[0]..(numbers[0] + @length - 1)))
+      1
+    else
+      2
+    end
+  end
 
     #Psuedo code:
     # 
@@ -90,51 +103,7 @@ class Validator
     #           if same, :vertical
     #           if not same, :horizontal 
     # 
-    # def in_range?(letters)
-    #     invalid_letters = 0
-    #     letter_range = ("A".."D").to_a           # this will be swapped to in order to upscale board
-
-    #     letters.each do |letter|
-    #         if !letter_range.include?(letter)
-    #             invalid_letters += 1
-    #         end 
-    #     end 
-
-    #     if invalid_letters > 0
-    #         false
-    #     else
-    #         true
-    #     end 
-    # end
-
-    # def validate_letters
-    #     letters = []
-    #     problems = 0
-    #     letters = @coordinates.each do |coordinate|
-    #         # coordinate is "A1"
-    #         letters << coordinate[0]
-    #     end 
-
-    #     if !in_range?(letters) 
-    #         problems += 1
-    #     end
-
-
-    # end 
-    # 
-    # number_validator that takes a coord []
-    #   number_validator extracts numbers into []
-    #       check range of number []
-    #       check numbers are all same or all incremental
-    #           if !same, check incremental by 1 only
-    #                   use each_cons()
-    #                   if arr[0].each_cons(arr.length) == arr
-    #                     return :horizontal = true
-    #       return a symbol, either :horizontal (same) or :vertical (incremental)
-    #           if same, :horizontal
-    #           if not same, :vertical
-    # 
-    # 
+    #     
     # 
     # check_coordinates method (user inputs are kosher)
     #   this method is used for coordinate validation, 
--- test/validator_test.rb
+++ test/validator_test.rb
@@ -3,31 +3,44 @@ require "Minitest/pride"
 require "./lib/validator"
 
 class ValidatorTest < Minitest::Test 
-    def test_it_exists
-        coordinates = ["A1", "A2", "A3"]
-        validator = Validator.new(coordinates, 3)
+  def test_it_exists
+      coordinates = ["A1", "A2", "A3"]
+      validator = Validator.new(coordinates, 3)
 
-        assert_instance_of Validator, validator
-        assert_equal 3, validator.length
-        assert_equal coordinates, validator.coordinates
-    end
+      assert_instance_of Validator, validator
+      assert_equal 3, validator.length
+      assert_equal coordinates, validator.coordinates
+  end
 
-    def test_length_method
-        coordinates = ["A1", "A2", "A3"]
-        validator = Validator.new(coordinates, 3)
-        validator2 = Validator.new(coordinates, 2)
+  def test_length_method
+      coordinates = ["A1", "A2", "A3"]
+      validator = Validator.new(coordinates, 3)
+      validator2 = Validator.new(coordinates, 2)
 
-        assert validator.length?
-        assert_equal false, validator2.length?
-    end 
+      assert validator.length?
+      assert_equal false, validator2.length?
+  end 
 
-    def test_letter_validator_works
-        coordinates = ["A1", "A2", "A3"]
-        validator = Validator.new(coordinates, 3)
-        validator2 = Validator.new(coordinates, 2)
+  def test_letter_validator_works
+      coordinates = ["A1", "A2", "A3"]
+      coordinates2 = ["A2", "A3", "B1"]
+      validator = Validator.new(coordinates, 3)
+      validator2 = Validator.new(coordinates2, 3)
 
-        assert_equal "vertical", validator.validate_letters
-    end
+      assert_equal 0, validator.validate_letters
+      assert_equal 2, validator2.validate_letters
+  end
+
+  def test_number_validation
+    coordinates = ["A1", "A2", "A3"]
+    coordinates2 = ["A2", "A3", "B1"]
+    validator = Validator.new(coordinates, 3)
+    validator2 = Validator.new(coordinates2, 3)
+
+    assert_equal 1, validator.validate_numbers
+    assert_equal 2, validator2.validate_numbers
+  end
+
 
     
 end 
