420df0baa5b3ed43ed3d1159d1c16db3b4192e11
Aiden Mendez <aidenmendez@gmail.com>
2020-10-19 07:19:29 -0600

    Test user has board when instantiated

---
+++ test/user_test.rb
@@ -0,0 +1,21 @@
+require "Minitest/autorun"
+require "Minitest/pride"
+require "./lib/user"
+require "./lib/board"
+require "./lib/cell"
+
+class TestUser < Minitest::Test
+  def setup
+    @user = User.new()
+  end
+
+  def test_user_player_class_exists
+    assert_instance_of User, @user
+  end
+
+  def test_user_had_board_when_initialized
+
+  end
+
+
+end
