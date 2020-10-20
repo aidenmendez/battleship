c8c2bc3ff25b6a7d8f74056226c8750c08d41c60
Max Ribbans <ribbans.max@gmail.com>
2020-10-19 22:04:12 -0400

    Fix .upcase placement to not error out

--- lib/user.rb
+++ lib/user.rb
@@ -15,11 +15,11 @@ class User
   def setup
     ships.each do |ship|
       puts "Enter the squares for the #{ship.name} (#{ship.length} spaces):"
-      coordinates = gets.chomp.split.upcase
+      coordinates = gets.chomp.upcase.split
 
       until board.valid_placement?(ship, coordinates) do
         puts "Those are invalid coordinates. Please try again:"
-        coordinates = gets.chomp.split.upcase
+        coordinates = gets.chomp.upcase.split
       end
       
       board.place(ship, coordinates)
@@ -34,4 +34,8 @@ class User
   def shot_at(coordinate)
     @board.cells[coordinate].fire_upon
   end 
+
+  def shot_result(coordinate)
+    @board.cells[coordinate].fired_result
+  end
 end
