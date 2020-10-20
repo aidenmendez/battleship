7645649ef4ef34db97803adeb75dc2051d5f31b4
Max Ribbans <ribbans.max@gmail.com>
2020-10-20 18:12:26 -0400

    Make turn method abstract render boards into own method fixed user_shot method report winner method

--- lib/controller.rb
+++ lib/controller.rb
@@ -35,15 +35,16 @@ class Controller
     end
 
     report_winner
+    reset_boards
     show_main_menu
   end
 
   def game_over?
     game_over = false
-    if @computer.board.render(true).!include?("S")
+    if !@computer.board.render(true).include?("S")
       @winner = "You"
       game_over = true
-    elsif @user.board.render(true).!include?("S")
+    elsif !@user.board.render(true).include?("S")
       @winner = "I"
       game_over = true
     end
@@ -51,6 +52,7 @@ class Controller
   end
 
   def report_winner
+    render_boards
     puts "#{winner} won. Womp womp."
   end
 
@@ -58,8 +60,10 @@ class Controller
     puts "Your turn. Enter the coordinate for your shot:"
     shot = gets.chomp.upcase
 
-    until @computer.board.valid_coordinate?(shot)
-      if @computer.board.cells[shot].fired_upon?
+    until @computer.board.valid_shot?(shot) && !@computer.board.cells[shot].fired_upon?
+      if @computer.board.cells[shot] == nil
+        puts "Please enter a valid coordinate:"
+      elsif @computer.board.cells[shot].fired_upon?
         puts "You already fired there. Numpty. Try again:"
       else
         puts "Please enter a valid coordinate:"
@@ -70,19 +74,35 @@ class Controller
     shot
   end
 
-  def turn
-    @computer.render_board
-    @user.render_board
-    @computer.shot_at(user_shot)
+  def random_shot
+    available_cells = @user.board.cells.select do |cell, cell_value|
+      !cell_value.fired_upon?
+    end
+    available_cells = available_cells.keys
+    available_cells.shuffle.shift
+  end
 
-    @user.shot_at(some computer method)
+  def display_shots(user_fires_at, computer_shot)
+    puts "You shot at #{user_fires_at}. It was a #{@computer.shot_result(user_fires_at)}."
+    puts "My shot on #{computer_shot} was a #{@user.shot_result(computer_shot)}."
 
   end
 
-  # Psuedo code for turns
-  # 
-  # 
-  # 
-  # puts and do computer takes a shot (not rum, or .whiskey)
-  # 
+  def render_boards
+    @computer.render_board
+    @user.render_board
+  end
+
+  def turn
+    render_boards
+    user_fires_at = user_shot 
+    @computer.shot_at(user_fires_at)
+    computer_shot = random_shot
+    @user.shot_at(computer_shot)
+    display_shots(user_fires_at, computer_shot)
+  end
+
+  def reset_boards 
+
+  end
 end
