d9e02cc2de8ce8163247fcfb38ad427ae3c14f7f
Max Ribbans <ribbans.max@gmail.com>
2020-10-19 20:49:51 -0400

    Add shot_at and render_board methods

--- lib/user.rb
+++ lib/user.rb
@@ -15,14 +15,23 @@ class User
   def setup
     ships.each do |ship|
       puts "Enter the squares for the #{ship.name} (#{ship.length} spaces):"
-      coordinates = gets.chomp.split
+      coordinates = gets.chomp.split.upcase
 
       until board.valid_placement?(ship, coordinates) do
         puts "Those are invalid coordinates. Please try again:"
-        coordinates = gets.chomp.split
+        coordinates = gets.chomp.split.upcase
       end
       
       board.place(ship, coordinates)
     end
   end
+
+  def render_board
+    puts "==============PLAYER BOARD=============="
+    puts @board.render(true)
+  end 
+
+  def shot_at(coordinate)
+    @board.cells[coordinate].fire_upon
+  end 
 end
